005d661e4ca2bf6da8c19bfb8059bad08c772e89
garymau <friden775@gmail.com>
2017-12-09 13:40:18 +0100

    changed regex search pattern, changed test_samples

--- issues/namelist.py
+++ issues/namelist.py
@@ -1,56 +1,55 @@
 import re
 
-
 names = [
-  "BRENT",
-  "CPI_ALCOHOL_rog",
-  "CPI_FOOD_rog",
-  "CPI_NONFOOD_rog",
-  "CPI_rog",
-  "CPI_SERVICES_rog",
-  "EXPORT_GOODS_bln_usd",
-  "GDP_bln_rub",
-  "GDP_yoy",
-  "GOV_EXPENSE_CONSOLIDATED_bln_rub",
-  "GOV_EXPENSE_FEDERAL_bln_rub",
-  "GOV_EXPENSE_SUBFEDERAL_bln_rub",
-  "GOV_REVENUE_CONSOLIDATED_bln_rub",
-  "GOV_REVENUE_FEDERAL_bln_rub",
-  "GOV_REVENUE_SUBFEDERAL_bln_rub",
-  "GOV_SURPLUS_FEDERAL_bln_rub",
-  "GOV_SURPLUS_SUBFEDERAL_bln_rub",
-  "IMPORT_GOODS_bln_usd",
-  "INDPRO_rog",
-  "INDPRO_yoy",
-  "INVESTMENT_bln_rub",
-  "INVESTMENT_rog",
-  "INVESTMENT_yoy",
-  "RETAIL_SALES_bln_rub",
-  "RETAIL_SALES_FOOD_bln_rub",
-  "RETAIL_SALES_FOOD_rog",
-  "RETAIL_SALES_FOOD_yoy",
-  "RETAIL_SALES_NONFOOD_bln_rub",
-  "RETAIL_SALES_NONFOOD_rog",
-  "RETAIL_SALES_NONFOOD_yoy",
-  "RETAIL_SALES_rog",
-  "RETAIL_SALES_yoy",
-  "TRANSPORT_FREIGHT_bln_tkm",
-  "UNEMPL_pct",
-  "USDRUR_CB",
-  "UST_10YEAR",
-  "UST_1MONTH",
-  "UST_1YEAR",
-  "UST_20YEAR",
-  "UST_2YEAR",
-  "UST_30YEAR",
-  "UST_3MONTH",
-  "UST_3YEAR",
-  "UST_5YEAR",
-  "UST_6MONTH",
-  "UST_7YEAR",
-  "WAGE_NOMINAL_rub",
-  "WAGE_REAL_rog",
-  "WAGE_REAL_yoy"
+    "BRENT",
+    "CPI_ALCOHOL_rog",
+    "CPI_FOOD_rog",
+    "CPI_NONFOOD_rog",
+    "CPI_rog",
+    "CPI_SERVICES_rog",
+    "EXPORT_GOODS_bln_usd",
+    "GDP_bln_rub",
+    "GDP_yoy",
+    "GOV_EXPENSE_CONSOLIDATED_bln_rub",
+    "GOV_EXPENSE_FEDERAL_bln_rub",
+    "GOV_EXPENSE_SUBFEDERAL_bln_rub",
+    "GOV_REVENUE_CONSOLIDATED_bln_rub",
+    "GOV_REVENUE_FEDERAL_bln_rub",
+    "GOV_REVENUE_SUBFEDERAL_bln_rub",
+    "GOV_SURPLUS_FEDERAL_bln_rub",
+    "GOV_SURPLUS_SUBFEDERAL_bln_rub",
+    "IMPORT_GOODS_bln_usd",
+    "INDPRO_rog",
+    "INDPRO_yoy",
+    "INVESTMENT_bln_rub",
+    "INVESTMENT_rog",
+    "INVESTMENT_yoy",
+    "RETAIL_SALES_bln_rub",
+    "RETAIL_SALES_FOOD_bln_rub",
+    "RETAIL_SALES_FOOD_rog",
+    "RETAIL_SALES_FOOD_yoy",
+    "RETAIL_SALES_NONFOOD_bln_rub",
+    "RETAIL_SALES_NONFOOD_rog",
+    "RETAIL_SALES_NONFOOD_yoy",
+    "RETAIL_SALES_rog",
+    "RETAIL_SALES_yoy",
+    "TRANSPORT_FREIGHT_bln_tkm",
+    "UNEMPL_pct",
+    "USDRUR_CB",
+    "UST_10YEAR",
+    "UST_1MONTH",
+    "UST_1YEAR",
+    "UST_20YEAR",
+    "UST_2YEAR",
+    "UST_30YEAR",
+    "UST_3MONTH",
+    "UST_3YEAR",
+    "UST_5YEAR",
+    "UST_6MONTH",
+    "UST_7YEAR",
+    "WAGE_NOMINAL_rub",
+    "WAGE_REAL_rog",
+    "WAGE_REAL_yoy"
 ]
 
 # part of issue https://github.com/mini-kep/db/blob/master/doc/listing.md
@@ -60,6 +59,7 @@ names = [
 
 concepts = dict(labor=['WAGE_*', 'UNEMPL'],
                 output=['IND*', 'TRANSPORT_FREIGHT'])
+
 
 # TODO 1: make a selection function that produces a list of
 #       variable names based on regex or variable head
@@ -74,19 +74,28 @@ concepts = dict(labor=['WAGE_*', 'UNEMPL'],
 def make_namelist(patterns):
     namelist = []
     search = ""
+
+    re_1 = re.compile(get_re())
     for p in patterns:
-        search += re.sub("[^\w_]+", "", p) + '|'
+        match = re_1.match(p)
+        search += match.group() + '|'
     search = search[:-1]
-    pattern = re.compile(str(search))
+
+    re_2 = re.compile(str(search))
     for name in names:
-        hit = pattern.search(name)
+        hit = re_2.search(name)
         if hit is not None:
             namelist.append(name)
     return namelist
 
+
+def get_re():
+    return '([A-Z]+_?)+'
+
+
 labor = make_namelist(concepts['labor'])
 assert set(labor) == set(["WAGE_NOMINAL_rub",
-  "WAGE_REAL_rog",
-  "WAGE_REAL_yoy",
-  "UNEMPL_pct"
-  ])
+                          "WAGE_REAL_rog",
+                          "WAGE_REAL_yoy",
+                          "UNEMPL_pct"
+                          ])
--- issues/test_namelist.py
+++ issues/test_namelist.py
@@ -2,33 +2,28 @@ import pytest
 
 from namelist import make_namelist
 
-patterns = [['GOV_REVENUE', 'GOV_SURPLUS'], ['GOV*'], ['CPI_SERVICES_rog']]
-results = [
-    [
-        "GOV_REVENUE_CONSOLIDATED_bln_rub",
-        "GOV_REVENUE_FEDERAL_bln_rub",
-        "GOV_REVENUE_SUBFEDERAL_bln_rub",
-        "GOV_SURPLUS_FEDERAL_bln_rub",
-        "GOV_SURPLUS_SUBFEDERAL_bln_rub",
-    ],
-    [
-        "GOV_EXPENSE_CONSOLIDATED_bln_rub",
-        "GOV_EXPENSE_FEDERAL_bln_rub",
-        "GOV_EXPENSE_SUBFEDERAL_bln_rub",
-        "GOV_REVENUE_CONSOLIDATED_bln_rub",
-        "GOV_REVENUE_FEDERAL_bln_rub",
-        "GOV_REVENUE_SUBFEDERAL_bln_rub",
-        "GOV_SURPLUS_FEDERAL_bln_rub",
-        "GOV_SURPLUS_SUBFEDERAL_bln_rub",
-    ],
-    ['CPI_SERVICES_rog'],
-
-]
+test_samples = {
+    ('GOV_REVENUE', 'GOV_SURPLUS'): ("GOV_REVENUE_CONSOLIDATED_bln_rub",
+                                     "GOV_REVENUE_FEDERAL_bln_rub",
+                                     "GOV_REVENUE_SUBFEDERAL_bln_rub",
+                                     "GOV_SURPLUS_FEDERAL_bln_rub",
+                                     "GOV_SURPLUS_SUBFEDERAL_bln_rub",),
+    ('GOV*',): ("GOV_EXPENSE_CONSOLIDATED_bln_rub",
+                "GOV_EXPENSE_FEDERAL_bln_rub",
+                "GOV_EXPENSE_SUBFEDERAL_bln_rub",
+                "GOV_REVENUE_CONSOLIDATED_bln_rub",
+                "GOV_REVENUE_FEDERAL_bln_rub",
+                "GOV_REVENUE_SUBFEDERAL_bln_rub",
+                "GOV_SURPLUS_FEDERAL_bln_rub",
+                "GOV_SURPLUS_SUBFEDERAL_bln_rub",),
+    ('CPI_SERVICES',): ('CPI_SERVICES_rog',)
+}
 
 
 def test_make_namelist_on_search_patterns_return_list_of_names_matching_patterns():
-    for test_i in range(len(patterns)):
-        assert set(make_namelist(patterns[test_i])) == set(results[test_i])
+    samples = test_samples.keys()
+    for sample in samples:
+        assert set(make_namelist(sample)) == set(test_samples[sample])
 
 
 if __name__ == "__main__":
